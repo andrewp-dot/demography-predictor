b805018da4e58a05dca26569c471d435c4e6de0c
Adnrew <ado.ponechal@gmail.com>
2025-02-08 22:01:49 +0100

    add StateDataLoader template for data preprocessing

--- src/local_model/model.py
+++ src/local_model/model.py
@@ -1,8 +1,13 @@
-from config import Config
+# Standart library imports
+from typing import Union
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 
 import pandas as pd
 import torch
 from torch import nn
+
+# Custom modules imports
+from config import Config
 
 # Set the seed for reproducibility
 torch.manual_seed(42)
@@ -16,7 +21,7 @@ torch.manual_seed(42)
 # 5. Implement the save and load functions
 
 
-class LoadStateData:
+class StateDataLoader:
 
     def __init__(self, state: str):
         self.state = state
@@ -29,12 +34,41 @@ class LoadStateData:
         data = pd.read_csv(f"{Config.states_data_dir}/{self.state}.csv")
         return data
 
+    def scale_data(
+        self,
+        data: pd.DataFrame,
+        scaler: Union[RobustScaler, StandardScaler, MinMaxScaler],
+    ) -> pd.DataFrame:
+        """Scales the data using the specified scaler.
+
+        Args:
+            data (pd.DataFrame): input data
+            scaler (Union[RobustScaler, StandardScaler, MinMaxScaler]): scaler to use
+
+        Returns:
+            pd.DataFrame: scaled data
+        """
+        raise NotImplementedError("scale_data method is not implemented yet.")
+
+    def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Scales and transforms the data for the specified format (3D tensor): `(batch_size,time_steps,input_features)`, where:
+        - batch_size: the number of samples processed in one forward/backward pass
+        - time_steps or sequence_length: number of time steps
+        - input_features: number of input features
+
+        :param data: pd.DataFrame
+        :return: pd.DataFrame
+        """
+        raise NotImplementedError("preprocess_data method is not implemented yet.")
+
 
 class LocalModel(nn.Module):
 
     def __init__(self, embedding_dim: int, hidden_dim: int):
         super(LocalModel, self).__init__()
         self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
 
     def train(self):
         raise NotImplementedError("train method is not implemented yet.")
