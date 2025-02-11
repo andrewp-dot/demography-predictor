08e4dd6357e9f98b2860cf21ab66f8bd64a6dfa1
Adnrew <ado.ponechal@gmail.com>
2025-02-11 21:22:59 +0100

    [WIP]: add custom splitting function for time-series data)

--- src/local_model/preprocessing.py
+++ src/local_model/preprocessing.py
@@ -1,10 +1,13 @@
 # Standard libraries imports
 import pandas as pd
-from typing import Union
+from typing import Union, Tuple
 from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 
 # Custom libraries imports
 from config import Config
+
+
+config = Config()
 
 
 class StateDataLoader:
@@ -17,7 +20,10 @@ class StateDataLoader:
         Load the state data
         :return: pd.DataFrame
         """
-        data = pd.read_csv(f"{Config.states_data_dir}/{self.state}.csv")
+        data = pd.read_csv(f"{config.states_data_dir}/{self.state}.csv")
+
+        # Lowercase columns
+        data.columns = data.columns.str.lower()
         return data
 
     def scale_data(
@@ -25,16 +31,34 @@ class StateDataLoader:
         data: pd.DataFrame,
         scaler: Union[RobustScaler, StandardScaler, MinMaxScaler],
     ) -> pd.DataFrame:
-        """Scales the data using the specified scaler.
+        """
+        Scales the data using the specified scaler.
 
-        Args:
-            data (pd.DataFrame): input data
-            scaler (Union[RobustScaler, StandardScaler, MinMaxScaler]): scaler to use
+        :param data: pd.DataFrame: input data
+        :param scaler: Union[RobustScaler, StandardScaler, MinMaxScaler]: scaler to use
 
         Returns:
             pd.DataFrame: scaled data
         """
         raise NotImplementedError("scale_data method is not implemented yet.")
+
+    def split_data(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Splits the data into train and test sets.
+
+        :param data: pd.DataFrame
+        :return: Tuple[pd.DataFrame, pd.DataFrame]: train and test data
+        """
+
+        # Sort data by year
+        data.sort_values(by="year", inplace=True)
+
+        # Split data
+        split_rate = 0.8
+        train_data = data.iloc[: int(split_rate * len(data))]
+        test_data = data.iloc[int(split_rate * len(data)) :]
+
+        return train_data, test_data
 
     def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
         """
@@ -47,3 +71,20 @@ class StateDataLoader:
         :return: pd.DataFrame
         """
         raise NotImplementedError("preprocess_data method is not implemented yet.")
+
+
+if __name__ == "__main__":
+
+    # Load czech data
+    czech_data_loader = StateDataLoader("Czechia")
+    czech_data = czech_data_loader.load_data()
+
+    train, test = czech_data_loader.split_data(czech_data)
+
+    print("-" * 100)
+    print("Train data tail:")
+    print(train.tail())
+
+    print("-" * 100)
+    print("Test data head:")
+    print(test.head())
