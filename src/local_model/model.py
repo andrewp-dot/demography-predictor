960098148f1145b02cd2db4701fb76620a145e40
Adnrew <ado.ponechal@gmail.com>
2025-02-11 21:05:12 +0100

    [WIP]: add basic implementation of local model

--- src/local_model/model.py
+++ src/local_model/model.py
@@ -86,6 +86,14 @@ class LSTMHyperparameters:
         )
 
 
+class TrainingStats:
+
+    def __init__(self):
+        self.losses = []
+        # self.mse = []
+        self.epochs = []
+
+
 class LocalModel(nn.Module):
 
     def __init__(self, hyperparameters: LSTMHyperparameters):
@@ -94,29 +102,25 @@ class LocalModel(nn.Module):
         self.hyperparameters: LSTMHyperparameters = hyperparameters
 
         # 3 layer model:
-        # 1. LSTM layer
-        lstm1 = nn.LSTMCell(
+        self.lstm = nn.LSTM(
             input_size=hyperparameters.input_size,
+            # Define the number of hidden units in the LSTM layer
             hidden_size=hyperparameters.hidden_size,
+            # Define number of layers of the neural LSTM network
+            num_layers=3,
+            # 'batch_first' indicates that the input is in the format (batch_size, sequence_length, input_features)
+            # not in the format (sequence_length, batch_size, input_features)
+            batch_first=True,
         )
 
-        # 2. LSTM layer
-        lstm2 = nn.LSTMCell(
-            input_size=hyperparameters.hidden_size,
-            hidden_size=hyperparameters.hidden_size,
-        )
-
-        # 3. LSTM layer
-        lstm3 = nn.LSTMCell(
-            input_size=hyperparameters.hidden_size,
-            hidden_size=hyperparameters.hidden_size,
-        )
-
-        # 4. Linear layer - output layer
-        linear = nn.Linear(
+        # Out layer: Linear layer
+        self.linear = nn.Linear(
             in_features=hyperparameters.hidden_size,
             out_features=hyperparameters.input_size,
         )
+
+        # Get stats
+        self.training_stats = TrainingStats()
 
     def __initialize_hidden_states(
         self, n_samples: int
@@ -139,21 +143,64 @@ class LocalModel(nn.Module):
         return h_t, c_t
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # define outputs
-        outputs = []
-        n_samples = x.size(0)  # or batch_size
+        # get the number batch size
+        n_samples = x.size(0)
 
         # initialize the hidden states
         h_t, c_t = self.__initialize_hidden_states(n_samples)
-        h_t2, c_t2 = self.__initialize_hidden_states(n_samples)
-        h_t3, c_t3 = self.__initialize_hidden_states(n_samples)
 
-        raise NotImplementedError("forward method is not implemented yet.")
+        # Forward propagate through LSTM
+        out, (h_n, c_n) = self.lstm(x, (h_t, c_t))  # In here 'out' is a tensor
 
-    def train(self):
-        raise NotImplementedError("train method is not implemented yet.")
+        out = self.linear(out[:, -1, :])  # Using the last time step's output
+
+        return out
+
+    def train(self, X_train: torch.Tensor, y_train: torch.Tensor):
+        # Define the loss function
+        criterion = nn.MSELoss()
+
+        # Define the optimizer
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.hyperparameters.learning_rate
+        )
+
+        # Define the training loop
+        num_epochs = self.hyperparameters.epochs
+
+        self.training_stats.epochs = list(range(num_epochs))
+        for epoch in range(num_epochs):
+            # Forward pass
+            self.train()
+
+            # Forward pass
+            outputs = self(X_train)
+            loss = criterion(outputs, y_train)
+
+            # get loss
+            self.training_stats.losses.append(loss.item())
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
 
 
 if __name__ == "__main__":
     # LSTM explanation - https://medium.com/analytics-vidhya/lstms-explained-a-complete-technically-accurate-conceptual-guide-with-keras-2a650327e8f2
-    pass
+
+    # Notes:
+    # statefull vs stateless LSTM - can I pass data with different batch sizes?
+
+    # TODO: transform this based on the data to be in the right format
+    hyperparameters = LSTMHyperparameters(
+        input_size=1,
+        hidden_size=100,
+        sequence_length=1,
+        learning_rate=0.001,
+        epochs=100,
+        batch_size=1,
+    )
+    rnn = LocalModel()
