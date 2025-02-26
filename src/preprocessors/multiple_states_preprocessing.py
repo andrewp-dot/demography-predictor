b935e2f4a3304bf6bb356dcee2c9a9d99f17a29a
Adnrew <ado.ponechal@gmail.com>
2025-02-26 16:29:22 +0100

    add split_data and scale_data method for StatesDataLoader and rewrite comments in different template

--- src/preprocessors/multiple_states_preprocessing.py
+++ src/preprocessors/multiple_states_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import logging
-from typing import Dict, List
+from typing import Dict, List, Tuple
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 
 from config import Config, setup_logging
 from src.preprocessors.state_preprocessing import StateDataLoader
@@ -41,9 +42,11 @@ class StatesDataLoader:
         """
         Loads the states from the given list
 
+        Args:
+            states (List[str]): List of state names which should be loaded to pandas dataframe.
 
-        :param states: List[str]: List of state names which should be loaded to pandas dataframe.
-        :return: Dict[str, pd.DataFrame]: The key is the state name and the value is state dataframe.
+        Returns:
+            Dict[str, pd.DataFrame]: The key is the state name and the value is state dataframe.
         """
 
         # Get loader for each state
@@ -61,7 +64,8 @@ class StatesDataLoader:
         """
         Loads data from all states.
 
-        :return: Dict[str, pd.DataFrame]: The key is the state name and the value is state dataframe.
+        Returns:
+            Dict[str, pd.DataFrame]: The key is the state name and the value is state dataframe.
         """
 
         # Get loader for each state
@@ -73,8 +77,74 @@ class StatesDataLoader:
         return state_dfs
 
     # TODO:
-    # implement method for splitting data to training and target sequences
-    # implement method for scaling data
+    # TEST THIS
+    def split_data(
+        self,
+        states_dict: Dict[str, pd.DataFrame],
+        sequence_len: int,
+        split_rate: float = 0.8,
+    ) -> Dict[str, Tuple[pd.DataFrame, pd.DataFrame]]:
+        """
+        Splits each dataframe to train and and test set. States with records * (1 - test_size) is lower then sequence len + 1 (length of the target sequence) are excluded.
+
+        Train data has to have length minimum of sequence len + 1 (to create target prediction sequence).
+
+        Args:
+            states_dict (Dict[str, pd.DataFrame]): Dictionary of loaded state dataframes. The dictionary key should be the state name.
+            sequence_len (int): The sequence length of the model. Need in order to check whether train and corresping target sequence can be created.
+            split_rate (float, optional): Percentage value of the training data.. Defaults to 0.8.
+
+        Returns:
+            Dict[str, Tuple[pd.DataFrame, pd.DataFrame]]: The 'state_name' is the key. Dict[state_name] = (train_data, test_data)
+        """
+
+        # Initialize state split dict
+        state_split_dict: Dict[str, Tuple[pd.DataFrame, pd.DataFrame]] = {}
+
+        # Split each state
+        for state_name, loader in self.__state_loaders.items():
+
+            # Get train data and test data
+            state_train_df, state_test_df = loader.split_data(
+                states_dict[state_name], split_rate=split_rate
+            )
+
+            # Check if training data have enough records
+            if len(state_train_df) <= sequence_len:
+                logger.warning(
+                    f"The training data of the state '{state_name}' does not have enough records to create training and target sequence. Therefore this state will be excluded from the training."
+                )
+                continue
+
+            # Save the state train and test data
+            state_split_dict[state_name] = tuple(state_train_df, state_test_df)
+
+        return state_split_dict
+
+    # TODO:
+    # Test scaling function
+    def scale_data(
+        self,
+        states_data: pd.DataFrame,
+        scaler: MinMaxScaler | RobustScaler | StandardScaler,
+    ) -> Tuple[pd.DataFrame, MinMaxScaler | RobustScaler | StandardScaler]:
+        """
+        Scales data for in the dafarame.
+
+        Args:
+            states_data (pd.DataFrame): Data to scale.
+            scaler (MinMaxScaler | RobustScaler | StandardScaler): Scaler which is used for data scaling.
+
+        Returns:
+            Tuple[pd.DataFrame, MinMaxScaler | RobustScaler | StandardScaler]: Scaled data, fitted scaler.
+        """
+        # Scale data
+        scaled_data = scaler.fit_transform(states_data)
+
+        # Create pandas dataframe from ndarray
+        scaled_data_df = pd.DataFrame(scaled_data, columns=states_data.columns)
+
+        return scaled_data_df, scaler
 
 
 if __name__ == "__main__":
