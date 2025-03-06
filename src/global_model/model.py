e08137f48b888aa7ee5f4347637dd257ea35ea90
Adnrew <ado.ponechal@gmail.com>
2025-03-06 21:33:47 +0100

    [WIP]: Demography predictor model interface - pt 2

--- src/global_model/model.py
+++ src/global_model/model.py
@@ -1,9 +1,82 @@
-import xgboost as xgb
+# Standard library imports
+import pandas as pd
+from typing import Union, List
+from pydantic import BaseModel
+
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+
+## Import models
+from xgboost import XGBRegressor
+
+# Custom imports
 
 # TODO: implement XGBoost using data
 
 
+class XGBoostTuneParams(BaseModel):
+    n_estimators: List[int]  # Number of boosting rounds
+    learning_rate: List[float]  # Step size shrinkage
+    max_depth: List[int]  # Depth of trees
+    subsample: List[float]  # Fraction of samples used per tree
+    colsample_bytree: List[float]  # Fraction of features used per tree
+
+
 class GlobalModel:
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        model: Union[XGBRegressor, GridSearchCV],
+        features: List[str],
+        targets: List[str],
+        params: XGBoostTuneParams | None = None,
+    ):
+
+        # Define model
+        self.model: Union[XGBRegressor] = model
+        self.params: XGBoostTuneParams = params
+
+        # Define features and targest
+        self.FEATURES: List[str] = features
+        self.TARGETS: List[str] = targets
+
+    def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        # First scale, sort, normalize data for input
+        raise NotImplementedError("")
+
+    def train_and_eval(
+        self, data: pd.DataFrame, split_size: float, tune_hyperparams: bool = False
+    ) -> None:
+
+        # Preprocess data
+        preprocessed_data = self.preprocess_data(data=data)
+
+        # Split data
+        X: pd.DataFrame = preprocessed_data[self.FEATURES]
+        y: pd.DataFrame = preprocessed_data[self.TARGETS]
+
+        X_train, y_train, X_test, y_test = train_test_split(
+            X, y, test_size=(1 - split_size)
+        )
+
+        # Tune hyperparams if neaded
+        if tune_hyperparams:
+            self.model = GridSearchCV(
+                estimator=self.model,
+                param_grid=self.params,
+                scoring="neg_mean_squared_error",  # Minimize MSE
+                cv=3,
+                verbose=1,
+                n_jobs=-1,  # Use all available CPUs
+            )
+
+        # Fit the model
+        self.model.fit(X_train, y_train)
+
+        # Training stats
+
+        raise NotImplementedError("")
+
+    def eval(self, test_df: pd.DataFrame) -> None:
+        NotImplementedError("")
--- src/predictors.py
+++ src/predictors.py
@@ -4,8 +4,10 @@ from typing import Union, List
 from xgboost import XGBRegressor
 
 # Custom imports
-from src.local_model.model import LocalModel
-from src.local_model.statistical_models import ARIMA
+from config import setup_logging
+from src.local_model.model import LSTMHyperparameters, LocalModel
+from src.global_model.model import GlobalModel
+from src.local_model.statistical_models import LocalARIMA
 
 
 # TODO: implement different models
@@ -16,30 +18,59 @@ class DemographyPredictor:
     def __init__(
         self,
         name: str,
-        local_model: Union[LocalModel, ARIMA],
-        global_model: Union[XGBRegressor],
+        local_model: Union[LocalModel, LocalARIMA],
+        global_model: Union[GlobalModel],
     ):
         # Define name of the model -> name is used to versionning etc
         self.name: str = name
 
         # Define architecture: local model -> global model
-        self.local_model: Union[LocalModel, ARIMA] = local_model
-        self.global_model: Union[XGBRegressor] = global_model
+        self.local_model: Union[LocalModel, LocalARIMA] = local_model
+        self.global_model: Union[GlobalModel] = global_model
 
     def __repr__(self) -> str:
-        return f"{self.name} ({type(self.local_model).__name__} -> {type(self.global_model).__name__})"
+        return f"{self.name} ({type(self.local_model).__name__} -> {type(self.global_model.model).__name__})"
 
     def predict(self, input_data: pd.DataFrame):
         raise NotImplementedError("Cannoct predict")
 
 
-class DemographyPredictorV1:
+def predictor_v1() -> DemographyPredictor:
 
-    def __init__(self):
-        pass
+    # Define features
+    FEATURES: List[str] = ["year"]
+    TARGET: str = "population, total"
+
+    # Define local model
+    hyperparameters = LSTMHyperparameters(
+        input_size=len(FEATURES + [TARGET]),
+        hidden_size=2048,
+        sequence_length=15,
+        learning_rate=0.0001,
+        epochs=30,
+        batch_size=1,
+        num_layers=4,
+    )
+
+    local_model = LocalModel(hyperparameters=hyperparameters)
+
+    # Define global model
+    global_model: GlobalModel = GlobalModel(model=XGBRegressor())
+
+    # Create predictor
+    predictor = DemographyPredictor(
+        "predictor_v1", local_model=local_model, global_model=global_model
+    )
+
+    return predictor
 
 
-class DemographyPRedictorV2:
+if __name__ == "__main__":
+    # Setup logging
+    setup_logging()
 
-    def __init__(self):
-        pass
+    # Create predictor
+    pred = predictor_v1()
+
+    # Print predictor
+    print(pred)
