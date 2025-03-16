51fb362849dcc69d94287f79f4d2666cf1af5249
Adnrew <ado.ponechal@gmail.com>
2025-03-16 21:58:27 +0100

    Multioutput regressor for XGBoost

--- src/global_model/model.py
+++ src/global_model/model.py
@@ -1,11 +1,13 @@
 # Standard library imports
 import pandas as pd
 import logging
-from typing import Union, List
+from typing import Union, List, Dict
 from pydantic import BaseModel
 
+from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputRegressor
 
 
 ## Import models
@@ -41,6 +43,22 @@ class XGBoostTuneParams(BaseModel):
     subsample: List[float]  # Fraction of samples used per tree
     colsample_bytree: List[float]  # Fraction of features used per tree
 
+    def to_regressor_dict(self, is_multitarget: bool) -> Dict[str, List[int | float]]:
+
+        # Get the original dict
+        original_dict = self.model_dump()
+
+        # Get the dict of the original
+        if not is_multitarget:
+            return original_dict
+
+        # Modify keys with 'estimator__' prefix
+        modified_dict: Dict[str, List[int | float]] = {}
+        for key, value in original_dict.items():
+            modified_dict[f"estimator__{key}"] = value
+
+        return modified_dict
+
 
 class GlobalModel:
 
@@ -53,12 +71,15 @@ class GlobalModel:
     ):
 
         # Define model
-        self.model: Union[XGBRegressor] = model
+        self.model: Union[XGBRegressor, MultiOutputRegressor] = model
         self.params: XGBoostTuneParams = params
 
         # Define features and targest
         self.FEATURES: List[str] = features
         self.TARGETS: List[str] = targets
+
+        # Initialize label encoders for categorical data
+        self.LABEL_ENCODERS: Dict[str, LabelEncoder] = {}
 
     def preprocess_data(
         self, data: pd.DataFrame, fitted_scaler: MinMaxScaler
@@ -77,12 +98,27 @@ class GlobalModel:
             scaled_numerical_data_array, columns=numerical_data_df.columns
         )
 
+        # Encode categorical data
         categorical_data_df = df.select_dtypes(exclude=["number"])
+
+        for col in categorical_data_df.columns:
+            new_label_encoder = LabelEncoder()
+
+            # Encode the data
+            categorical_data_df[col] = new_label_encoder.fit_transform(
+                categorical_data_df[col]
+            )
+
+            # Save the encoder for the column
+            self.LABEL_ENCODERS[col] = new_label_encoder
 
         scaled_df = pd.concat([categorical_data_df, scaled_numerical_data_df], axis=1)
 
         return scaled_df
 
+    # TODO:
+    # Preprocess categorical data
+    # Delete DMatricies -> in order to support parameter tuning and multioutput regression
     def train_and_eval(
         self,
         data: pd.DataFrame,
@@ -90,6 +126,9 @@ class GlobalModel:
         fitted_scaler: MinMaxScaler,
         tune_hyperparams: bool = False,
     ) -> None:
+
+        # Find out whether training multioutput regressor
+        IS_MULTI_TARGET = len(self.TARGETS) > 1
 
         # Set the country name as the categorical column
         if "country name" in data.columns:
@@ -99,8 +138,6 @@ class GlobalModel:
         logger.info("Preprocessing data....")
         preprocessed_data = self.preprocess_data(data=data, fitted_scaler=fitted_scaler)
 
-        logger.critical(preprocessed_data.columns)
-
         # Split data
         X: pd.DataFrame = preprocessed_data[self.FEATURES]
         y: pd.DataFrame = preprocessed_data[self.TARGETS]
@@ -109,34 +146,38 @@ class GlobalModel:
             X, y, test_size=(1 - split_size), random_state=42
         )
 
-        # Convert dataset into DMatrix format
-        # if len(self.TARGETS) > 1:
-        #     raise NotImplementedError("Multitarget training not implemented yet!")
+        # Create multioutput regressor for for the targets
+        param_dict = None
 
-        # Create train and test DMatricies
-        dtrain = xgb.DMatrix(X_train, label=y_train, enable_categorical=True)
-        dtest = xgb.DMatrix(X_test, label=y_test, enable_categorical=True)
+        if self.params:
+            param_dict = self.params.to_regressor_dict(is_multitarget=IS_MULTI_TARGET)
+
+        # Create multioutput regressor for for the targets
+        if IS_MULTI_TARGET:
+            self.model = MultiOutputRegressor(self.model)
 
         # Tune hyperparams if neaded
-        if tune_hyperparams and self.params is not None:
-            logger.info("Tuning parameters....")
-            self.model = GridSearchCV(
-                estimator=self.model,
-                param_grid=self.params,
-                scoring="neg_mean_squared_error",  # Minimize MSE
-                cv=3,
-                verbose=1,
-                n_jobs=-1,  # Use all available CPUs
-            )
+        if tune_hyperparams:
+
+            if param_dict is not None:
+                logger.info("Tuning parameters....")
+                self.model = GridSearchCV(
+                    estimator=self.model,
+                    param_grid=param_dict,
+                    scoring="neg_mean_squared_error",  # Minimize MSE
+                    cv=3,
+                    verbose=1,
+                    n_jobs=2,  # Use all available CPUs
+                )
+            else:
+                logger.warning(
+                    f"Tune hyperparameters is set to True, but no hyperparameters were provided. Skipping parameter tuning."
+                )
 
         # Fit the model
         logger.info("Fitting model...")
 
-        params = {"objective": "reg:squarederror", "eval_metric": "rmse"}
-
-        self.model = xgb.train(
-            params, dtrain, num_boost_round=100, evals=[(dtest, "Test")]
-        )
+        self.model.fit(X_train, y_train)
 
     def eval(self, test_df: pd.DataFrame) -> None:
         NotImplementedError("")
@@ -149,12 +190,12 @@ def try_single_target_global_model():
     logger.info("Loading whole dataset....")
 
     # Targets
-    # targets: List[str] = ["population, total"]
-    targets: List[str] = [
-        "population ages 15-64",
-        "population ages 0-14",
-        "population ages 65 and above",
-    ]
+    targets: List[str] = ["population, total"]
+    # targets: List[str] = [
+    #     "population ages 15-64",
+    #     "population ages 0-14",
+    #     "population ages 65 and above",
+    # ]
 
     # Features
     features: List[str] = [
@@ -162,23 +203,24 @@ def try_single_target_global_model():
     ]
 
     # Tune params
+    # Note: too many parameters results in a warning
     tune_parameters = XGBoostTuneParams(
-        n_estimators=[50, 100, 200],
-        learning_rate=[0.001, 0.01, 0.05, 0.1],
-        max_depth=[3, 5, 7, 10],
-        subsample=[0.5, 0.7, 0.9, 1.0],
+        n_estimators=[50, 100],
+        learning_rate=[0.001, 0.01, 0.05],
+        max_depth=[3, 5, 7],
+        subsample=[0.5, 0.7],
         colsample_bytree=[0.5, 0.7, 0.9, 1.0],
     )
 
     # Create global model
     gm = GlobalModel(
-        model=XGBRegressor(),
+        model=XGBRegressor(objective="reg:squarederror", random_state=42),
         features=features,
         targets=targets,
         params=tune_parameters,
     )
 
-    logger.info("Training and evaluation model....")
+    logger.info("Training and evaluating the model....")
 
     # Simulation of scaler used to scale the data from the local model
     scaler = MinMaxScaler()
